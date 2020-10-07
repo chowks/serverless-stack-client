e2c4c94960c505d41b4e48b5c8b7e33fcd344298
chowks <chowks05@gmail.com>
2020-10-07 16:57:15 +0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "ap-southeast-1",
+        BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-y04pcbf6c6ji"
+    },
+    apiGateway: {
+        REGION: "ap-southeast-1",
+        URL: "https://obuqo7f074.execute-api.ap-southeast-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "ap-southeast-1",
+        USER_POOL_ID: "ap-southeast-1_sqtNJclDy",
+        APP_CLIENT_ID: "7nmcjsr17kees90eb7lnn2j4u0",
+        IDENTITY_POOL_ID: "ap-southeast-1:0eeeea2e-71f8-4e6d-b35e-f09630377108"
+    }
+};
